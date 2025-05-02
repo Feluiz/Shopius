1315df6f8c13ce2bdf7013b38d54db388a8fcfd5
Feluiz <138745247+Feluiz@users.noreply.github.com>
2025-05-02 15:51:04 -0600

    update: Added axios and implement API call

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   return (
     <>
       <div className='bg-black'>Hello World!</div>
+      <button onClick={fetchData}>Fetch Data</button>
     </>
   )
 }
